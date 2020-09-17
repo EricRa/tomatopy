40426483a6befa7bf8680d3e027d35e5f5a5ff0a
Eric Ransom <ransom.eric@gmail.com>
2020-09-16 23:53:22 -0400

    trying to get tkinter to place nice with countdown timer

--- src/backup - main.py
+++ src/backup - main.py
@@ -12,35 +12,24 @@ window.geometry("400x200")
 def clicked():
     print("click!")
 
-#Countdown timer and button functions
-
-clock = int(1500)
-time_label = Label(window, text = clock)
-time_label.place(x=100, y=100)
-    
-def start_timer():   
+#Countdown timer
+def timer():
     initial_time = 1500
     while initial_time != 0:
             minutes, seconds = divmod(initial_time, 60)
             clock = "{:02d}:{:02d}".format(minutes, seconds)
-            time_label.grid()
+            print(clock)
             time.sleep(1)
             initial_time = initial_time - 1
 
-def stop_timer():
-    pass
-
-def reset_timer():
-    pass
-
 # Buttons here
-start = Button(window, text="Start", bg="grey", fg="white", command=start_timer)
+start = Button(window, text="Start", bg="grey", fg="white", command=start)
 start.grid(column=2, row=2)
 
-stop = Button(window, text="Stop", bg="grey", fg="white", command=stop_timer)
+stop = Button(window, text="Stop", bg="grey", fg="white", command=clicked)
 stop.grid(column=4, row=2)
 
-reset = Button(window, text="Reset", bg="grey", fg="white", command=reset_timer)
+reset = Button(window, text="Reset", bg="grey", fg="white", command=clicked)
 reset.grid(column=6, row=2)
 
 
