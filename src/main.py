27a44ae193eafbfb428c5f3f816792bf461323e9
Eric Ransom <ransom.eric@gmail.com>
2020-09-17 11:01:10 -0400

    Finished tentative tkinter layout.  Loop logic a work in progress

--- src/main.py
+++ src/main.py
@@ -4,9 +4,10 @@ from tkinter import Menu
 import time
 
 # Creates window and defines properties
-window = Tk()
-window.title("Python Tomato Timer v0.1")
-window.geometry("400x200")
+root = Tk()
+root.title("Python Tomato Timer v0.1")
+root.geometry("500x300")
+root.config(background="black")
 
 #Button Functions go here
 def clicked():
@@ -15,8 +16,10 @@ def clicked():
 #Countdown timer and button functions
 
 clock = int(1500)
-time_label = Label(window, text = clock)
-time_label.place(x=100, y=100)
+time_label = Label(root, text = clock)
+time_label.place(x=150, y=138)
+time_label.config(font=("Courier", 50, "bold"))
+time_label.config(background="black", foreground="white")
     
 def start_timer():   
     initial_time = 1500
@@ -34,14 +37,20 @@ def reset_timer():
     pass
 
 # Buttons here
-start = Button(window, text="Start", bg="grey", fg="white", command=start_timer)
+start = Button(root, text="Start", bg="grey", fg="white", command=start_timer)
 start.grid(column=2, row=2)
+start.config(font=("Courier", 34))
+start.config(background = "green", foreground = "white")
 
-stop = Button(window, text="Stop", bg="grey", fg="white", command=stop_timer)
+stop = Button(root, text="Stop", bg="grey", fg="white", command=stop_timer)
 stop.grid(column=4, row=2)
+stop.config(font=("Courier", 34))
+stop.config(background = "red", foreground = "white")
 
-reset = Button(window, text="Reset", bg="grey", fg="white", command=reset_timer)
+reset = Button(root, text="Reset", bg="grey", fg="white", command=reset_timer)
 reset.grid(column=6, row=2)
+reset.config(font=("Courier", 34))
+reset.config(background = "#a8b548", foreground = "white")
 
 
-window.mainloop()
+root.mainloop()
